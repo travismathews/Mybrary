04c5a61f5dbe56d4327a78167a559ce419e2b684
Travis Mathews <travis@travismathews.io>
2021-06-18 19:37:38 -0400

    Added Unifiedtopology flag per MongoDB Deprecation warning

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ app.use(express.static('public'));
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true});
+mongoose.connect(process.env.DATABASE_URL, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
